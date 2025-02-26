ce428732757312f028bfc1fc8481dfa8a0189577
harshsaxena40601 <146015392+harshsaxena40601@users.noreply.github.com>
2025-02-26 12:19:18 +0530

    a

--- ecommerce_backend/ecommerce_backend/settings.py
+++ ecommerce_backend/ecommerce_backend/settings.py
@@ -79,7 +79,7 @@ DATABASES = {
 STATIC_URL = "/static/"
 STATIC_ROOT = BASE_DIR / "staticfiles"
 STATICFILES_DIRS = [BASE_DIR / "static"]
-
+STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 MEDIA_URL = "/media/"  # ✅ Fixed: Use relative URL
 MEDIA_ROOT = BASE_DIR / "media"
 
